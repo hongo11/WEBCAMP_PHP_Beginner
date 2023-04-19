5c6a98ef91be58538451805f21a8a63b8f4d69f9
ryo <hanauta0226@gmail.com>
2023-04-19 08:31:23 +0000

    my_function.phpを書いた

---
+++ my_function.php
@@ -0,0 +1,30 @@
+<?php
+
+// 「引数の値を２倍にする」関数
+function numToDouble(int $i): int // 今回書き換えた箇所　その１
+{
+    // 値を２倍にする
+    $ret = $i * 2;
+    
+    // 戻り値
+    return $ret;
+}
+
+//
+$num = numToDouble(10);
+echo "10 を２倍にしたら {$num} になった \n";
+
+// ２つの引数を「加算する」関数
+function numAdd(int $a, int $b): int // 今回書き換えた箇所　その２
+{
+    // 加算する
+    $ret = $a + $b;
+    
+    // 戻り値
+    return $ret;
+}
+
+//
+$add = numAdd(5, 6);
+echo "5 と 6を足したら {$add} になった \n";
+
