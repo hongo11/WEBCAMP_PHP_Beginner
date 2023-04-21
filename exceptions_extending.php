438e46c466d2c3fa79cd02958e69a24ee74b06be
ryo <hanauta0226@gmail.com>
2023-04-21 22:40:52 +0000

    multi_catch.phpを書いた

--- exceptions_extending.php
+++ exceptions_extending.php
@@ -8,10 +8,10 @@ class MyException extends Exception
 try {
     // 自力で例外を発生させる（例外を投げる）
     throw new \MyException("自力で投げた例外");
-    
+
     // 例外を発生させる
     $obj = new SplFileObject('dummy');
-    
+
     // その後の処理（動かないことを確認するため）
     echo "例外が起きた後の処理。ここは通る？ \n";
 } catch(\Throwable $e) {
